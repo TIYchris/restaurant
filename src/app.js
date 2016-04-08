a6b493ae81810ae416c489d589fca856b8c0055e
snowgoing <snowgoing@gmail.com>
2016-04-07 17:34:54 -0700

    fixing conflicts

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
+
+
+// Layout
+import App from 'layouts/app';
+
+// Components
+import Home from "ui/home";
+
+render(
+  <Router history={browserHistory}>
+
+
+    <Route path="/" component={App}></Route>
+
+    
+  </Router>
+,document.getElementById('app'));
---
+++ src/layouts/app.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router';
+require('assets/styles/styles.scss');
+
+
+export default React.createClass({
+  render: function () {
+    return (
+      <div className="app">
+        <div className="header">
+
+
+        <img src="images/rocky-slims-logo-big.png" />
+          <div id="social">
+            <ul id="icons">
+              <li className="zocial-facebook"></li>
+              <li className="zocial-twitter"></li>
+              <li className="zocial-instagram"></li>
+              <li className="zocial-opentable"></li>
+            </ul>
+          </div>  
+
+        <img src="images/rocky-slims-logo-big.png" />
+
+        </div>
+        {this.props.children}
+        <div id="footer">
+          <p>&copy; 2016 TIY</p>
+        </div>
+      </div>
+    );
+  }
+})
