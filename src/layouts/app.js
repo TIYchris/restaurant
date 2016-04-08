2d1843b16a89c3e6d8cc32e79e098827421de11a
snowgoing <snowgoing@gmail.com>
2016-04-08 15:02:47 -0700

    Add Function

--- src/layouts/app.js
+++ src/layouts/app.js
@@ -4,6 +4,7 @@ import Banner from 'ui/bannerImageComponent';
 import Social from "ui/social-media";
 import Special from "ui/special";
 import Maps from "ui/google-maps";
+import Blog from "ui/blog";
 
 require('assets/styles/styles.scss');
 
@@ -14,11 +15,15 @@ export default React.createClass({
       <div className="app">
         <div className="header">
           <img id="logo"src="images/rocky-slims-logo-big.png" />
-            <Social/>
-            <Banner/>
-            <Special/>
+              <Social/>
+              <Banner/>
+              <div className="midBar">
+                <Blog/>
+                <Special/>
+                <Maps/>
+              </div>
         </div>
-        <Maps/>
+        
         {this.props.children}
         <div id="footer">
           <p>&copy; 2016 Rocky Slims</p>
--- src/ui/blog.js
+++ src/ui/blog.js
@@ -1,13 +1,13 @@
 import React from 'react';
 require('assets/styles/blog.scss');
 
-export default React.createClass(){
+export default React.createClass({
 	render: function(){
 		return (
 		<div className="blog">
 			<h3>Latest News</h3>
-			<div className="blogFeed">{this.props.data.blog}<span>read more ></span></div>
+			<div className="blogFeed"><span>read more ></span></div>
 		</div>
 		)
 	}
-}
+});	
